683e3be59363ece439fc3ecc065224c70adf6bd8
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-10-30 12:58:13 -0400

    Initial Commit

---
+++ ProjectCOP4655/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectCOP4655
+//
+//  Created by Emin Mammadzada on 10/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
